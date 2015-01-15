8f31ef48f14d01644b28d98e4e7d52311a9e4a12
Jonathan Cobb <misterjcobb@gmail.com>
2015-01-15 15:19:54 -0700

    more filtering

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,29 +1,30 @@
 'use strict';
 
 angular.module('myApp.controllers', [])
-.controller('homeCtrl', ['$scope', function($scope) {
-	
-}])
-.controller('cdsCtrl', ['$scope', '$filter', 'getData', function($scope, $filter, getData) {
+.controller('mediaCtrl', ['$scope', '$filter', 'getData', function($scope, $filter, getData) {
+	$scope.getDataByType = function(type){
 	var promise = getData.getAll();
 	 promise.then(
           function(payload) { 
-              $scope.items = $filter('byType')(payload.data, "cd");
+              $scope.items = $filter('byType')(payload.data, type);
           },
           function(errorPayload) {
               $log.error('failure loading test data', errorPayload);
           });
+	}
+}])
+.controller('homeCtrl', ['$scope', '$controller', function($scope, $controller) {
+	$controller('mediaCtrl', {$scope: $scope});
+	$scope.getDataByType('all');
+}])
+.controller('cdsCtrl', ['$scope', '$controller', function($scope, $controller) {
+	$controller('mediaCtrl', {$scope: $scope});
+	$scope.getDataByType('cd');
 
 
 }])
-.controller('dvdsCtrl', ['$scope', '$filter', 'getData', function($scope, $filter, getData) {
-	var promise = getData.getAll();
-	 promise.then(
-          function(payload) { 
-              $scope.items = $filter('byType')(payload.data, "dvd");
-          },
-          function(errorPayload) {
-              $log.error('failure loading test data', errorPayload);
-          });
+.controller('dvdsCtrl', ['$scope', '$controller', function($scope, $controller) {
+	$controller('mediaCtrl', {$scope: $scope});
+	$scope.getDataByType('dvd');
 
 }]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -4,14 +4,18 @@
 
 angular.module('myApp.filters', [])
 .filter('byType', function () {
-  return function (items, type) {
-    var filtered = [];
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      if (item.type==type) {
-        filtered.push(item);
-      }
-    }
-    return filtered;
-  };
+	return function (items, type) {
+		var types = ['cd','dvd','ebook'];
+		if(types.indexOf(type)!=-1 ){
+			var filtered = [];
+			for (var i = 0; i < items.length; i++) {
+				var item = items[i];
+				if (item.type==type) {
+					filtered.push(item);
+				}
+			}
+			return filtered;
+		}
+		return items;
+	};
 });
