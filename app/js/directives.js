646790020fc2d47a39cf52ccd7a56beb2cb48bbb
Jonathan Cobb <misterjcobb@gmail.com>
2015-01-20 14:42:21 -0700

    track level validation

--- app/js/directives.js
+++ app/js/directives.js
@@ -79,31 +79,31 @@ angular.module('myApp.directives', [])
 .directive('filename', function() {
   return {
     restrict: 'A',
-    template: '<span>{{key}}<textarea ng-readonly="!editMode" ng-bind="key" ng-model="value" ng-change="addEdit(key, value)"></textarea></span>'
+    templateUrl: 'views/directives/filename.html'
   }
 })
 .directive('trackId', function() {
   return {
     restrict: 'A',
-    template: '<span>{{key}}<textarea ng-readonly="!editMode" ng-bind="key" ng-model="value" ng-change="addEdit(key, value)"></textarea></span>'
+    templateUrl: 'views/directives/trackId.html'
   }
 })
 .directive('trackName', function() {
   return {
     restrict: 'A',
-    template: '<span>{{key}}<textarea ng-readonly="!editMode" ng-bind="key" ng-model="value" ng-change="addEdit(key, value)"></textarea></span>'
+    templateUrl: 'views/directives/trackName.html'
   }
 })
 .directive('trackNumber', function() {
   return {
     restrict: 'A',
-    template: '<span>{{key}}<textarea ng-readonly="!editMode" ng-bind="key" ng-model="value" ng-change="addEdit(key, value)"></textarea></span>'
+    templateUrl: 'views/directives/trackNumber.html'
   }
 })
 .directive('trackTime', function() {
   return {
     restrict: 'A',
-    template: '<span>{{key}}<textarea ng-readonly="!editMode" ng-bind="key" ng-model="value" ng-change="addEdit(key, value)"></textarea></span>'
+    templateUrl: 'views/directives/trackTime.html'
   }
 })
 
@@ -113,38 +113,26 @@ angular.module('myApp.directives', [])
 // Validation directives
 
 .directive("validateRuntime", function(){
-          // requires an isloated model
           return {
-           // restrict to an attribute type.
            restrict: 'A',
-          // element must have ng-model attribute.
            require: 'ngModel',
            link: function(scope, ele, attrs, ctrl){
-
-              // add a parser that will process each time the value is
-              // parsed into the model when the user updates it.
               ctrl.$parsers.unshift(function(value) {
                 if(value){
-
                   var valid = false;
                 	var values = value.split(":");
                 	if(values.length==3){
-
                 		valid=true;
                 		for(var i=0; i<values.length; i++){
-                			if(!(values[i].length==2 && angular.isNumber(parseInt(values[i])))) {
+                			if(!(values[i].length==2 && !isNaN(parseInt(values[i])))) {
                 				valid=false;
                 			}
                 		}
                 	}
                   ctrl.$setValidity('invalidRuntime', valid);
                 }
-
-                // if it's valid, return the value to the model,
-                // otherwise return undefined.
                 return valid ? value : undefined;
               });
-
            }
           }
         })
