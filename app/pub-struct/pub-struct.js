cd92e252151223b03d319b72264e301eb8d9d05b
Jonathan Cobb <misterjcobb@gmail.com>
2015-01-27 12:00:54 -0700

    got names in pub struct

--- app/pub-struct/pub-struct.js
+++ app/pub-struct/pub-struct.js
@@ -3,33 +3,66 @@
 angular.module('myApp.pubStruct', [])
 .controller('pubStructCtrl', ['$scope', 'getData', function($scope, getData){
 	
+	var getItems= function(){
+	var promise = getData.getAll();
+		promise.then(
+			function(payload){
+				$scope.items=payload.data;
+				getStruct();
+			},
+			function(errorPayload){
+				$log.error('failure loading data', errorPayload);
+			});
+	}
 
-	var promise = getData.getPubStruct();
-	promise.then(
-		function(payload) { 
-			$scope.ps=payload.data;
-			console.log($scope.ps);
-		},
-		function(errorPayload) {
-			$log.error('failure loading publication structure', errorPayload);
-		});
+	var getStruct = function(){
+		var promise = getData.getPubStruct();
+		promise.then(
+			function(payload) { 
+				$scope.ps=payload.data;
+			},
+			function(errorPayload) {
+				$log.error('failure loading publication structure', errorPayload);
+			});
+	}
+
+
+	getItems();
+	
 
 }])
+.filter('pubStructFilter', function(){
 
+
+	return function (ids, items){
+		var filtered = [];
+		angular.forEach(ids, function(id){
+					for(var i=0; i<items.length; i++){
+						var item = items[i];
+						if(item["id"] == id){
+							filtered.push(item["title"]);
+						}
+					}
+				});
+				return filtered;
+			}
+		
+})
 
 .directive('recursiveStructure', function(recursionHelper) {
-  return {
-    restrict: 'E',
-    required: 'category',
-    scope:{
-    	category: "="
-    },
+	return {
+		restrict: 'E',
+		required: 'category',
+		scope:{
+			category: "=",
+			items: "="
+		},
 
-    templateUrl: 'pub-struct/recursive-structure.html',
-    compile: function(element) {
+		templateUrl: 'pub-struct/recursive-structure.html',
+		compile: function(element) {
             // Use the compile function from the RecursionHelper,
             // And return the linking function(s) which it returns
             return recursionHelper.compile(element);
+        }
     }
-  }
 });
