c12479dd8eb150845b6416c3beee47fe83b27239
Jonathan Cobb <misterjcobb@gmail.com>
2015-01-28 17:38:12 -0700

    I did it\! I created my own sortable tree structure

--- app/pub-struct/pub-struct.js
+++ app/pub-struct/pub-struct.js
@@ -201,25 +201,29 @@ angular.module('myApp.pubStruct', ['ui.sortable'])
 
 			        	scope.$apply(function(){
 			        		
-			        		if(!scope.category.pubDropIndex){
-			        			scope.category.pubDropIndex = scope.category.publications.length;
+
+			        		//add the new item at the right place, saving that index
+			        		if(!scope.category.dropIndex){
+			        			scope.category.dropIndex = scope.category.publications.length;
 			        			scope.category.publications.push(item.id);
 			        		}else{
-			        			scope.category.publications.splice(scope.category.pubDropIndex, 0, item.id);
+			        			scope.category.publications.splice(scope.category.dropIndex, 0, item.id);
 			        		}
 
-			        		
+
+			        		//find any duplicates (there should only be one)
 			        		var dupIndex = null
 			        		for(var i=0; i<scope.category.publications.length; i++){
-			        			if(scope.category.publications[i] == item.id && i!=scope.category.pubDropIndex){
-			        				// 
+			        			if(scope.category.publications[i] == item.id && i!=scope.category.dropIndex){
 			        				dupIndex=i;
 			        			}
 			        		}
+
+			        		//delete duplicate if any
 			        		if(dupIndex!=null){
-			        				scope.category.publications.splice(dupIndex,1);
+			        			scope.category.publications.splice(dupIndex,1);
 			        		}
-			        		delete scope.category.pubDropIndex;
+			        		delete scope.category.dropIndex;
 
 
 			        	});
@@ -228,18 +232,50 @@ angular.module('myApp.pubStruct', ['ui.sortable'])
 			        }else if(item.hasOwnProperty('subcategories')){
 			        	// console.log("category: " + item.title+ " dropped in category: " +scope.category.title);
 			        	
-			        	var noDuplicate = true;
-			        	for(var i=0; i<scope.category.subcategories.length; i++){
-			        		if(scope.category.subcategories[i].title == item.title){
-			        			noDuplicate = false;
-			        		}
+
+			        	scope.$apply(function(){
+
+
+
+			        	//add the new item at the right place, saving that index
+			        	if(!scope.category.dropIndex){
+			        		console.log('no drop index');
+			        		scope.category.dropIndex = scope.category.subcategories.length;
+			        		scope.category.subcategories.push(item);
+			        	}else{
+			        		console.log('drop index: ' + scope.category.dropIndex);
+			        		scope.category.subcategories.splice(scope.category.dropIndex, 0, item);
 			        	}
 
-			        	if(noDuplicate && item.title!=scope.category.title){
-			        		scope.$apply(function(){
-			        			scope.category.subcategories.push(item);	
-			        		});
-			        	}
+
+			        		//find any duplicates (there should only be one)
+			        		var dupIndex = null
+			        		for(var i=0; i<scope.category.subcategories.length; i++){
+			        			if(scope.category.subcategories[i].title == item.title && i!=scope.category.dropIndex){
+			        				dupIndex=i;
+			        			}
+			        		}
+
+			        		//delete duplicate if any
+			        		if(dupIndex!=null){
+			        			scope.category.subcategories.splice(dupIndex,1);
+			        		}
+			        		delete scope.category.dropIndex;
+
+			        	});
+
+			        	// var noDuplicate = true;
+			        	// for(var i=0; i<scope.category.subcategories.length; i++){
+			        	// 	if(scope.category.subcategories[i].title == item.title){
+			        	// 		noDuplicate = false;
+			        	// 	}
+			        	// }
+
+			        	// if(noDuplicate && item.title!=scope.category.title){
+			        	// 	scope.$apply(function(){
+			        	// 		scope.category.subcategories.push(item);	
+			        	// 	});
+			        	// }
 			        	
 
 			        }
@@ -282,7 +318,8 @@ angular.module('myApp.pubStruct', ['ui.sortable'])
 			el.addEventListener(
 				'drop',
 				function(e) {
-					scope.parentCategory.pubDropIndex = scope.index;
+					scope.parentCategory.dropIndex = scope.index;
+					console.log(scope.parentCategory.dropIndex);
 					this.classList.remove('sorta-over');
 					return false;
 				},
