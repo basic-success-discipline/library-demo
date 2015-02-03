e961aa895b5135b07e511041e196b7fc48cb29bb
Jonathan Cobb <misterjcobb@gmail.com>
2015-02-02 17:12:04 -0700

    mkay I think I've gotten it contained in a service now

--- app/pub-struct/AL-sortable/AL-sortable.js
+++ app/pub-struct/AL-sortable/AL-sortable.js
@@ -1,5 +1,73 @@
 angular.module('al.sortable', [])
 
+
+.service('dragDrop', function(){
+	var drag = {pub:null,category:null,parent:null,type:null};
+	var drop = {category: null};
+	var sortaDrop = {index: null, category:null};
+	var vd = true;
+	return {
+		setDragVars: function(pub, cat, parent, type){
+			drag.pub = pub;
+			drag.category = cat;
+			drag.parent = parent;
+			drag.type = type;
+		},
+		getDragItem : function(){
+			if(drag.type=="pub"){
+				return drag.pub;
+			}
+			else if(drag.type=="category"){
+				return drag.category;
+			}
+		},
+		setDropVars : function(cat){
+			drop.category = cat;
+		},
+		setSortaDropVars : function(index, category){
+			sortaDrop.index = index;
+			sortaDrop.category = category; 
+		},
+		getDrag : function(){
+			return drag;
+		},
+		getDrop : function(){
+			return drop;
+		},
+		getSortaDrop : function(){
+			return sortaDrop;
+		},
+		
+		isValidDrop : function(rdrag, rdrop){
+			var validDrop = true;
+			if(rdrag.title == rdrop.title){
+				validDrop = false;
+			}else{
+				for(var i=0; i<rdrag.subcategories.length; i++){
+
+					if(validDrop){
+						validDrop = this.isValidDrop(rdrag.subcategories[i], rdrop );
+					}
+				}
+			}
+			vd = validDrop;
+			return validDrop;
+		},
+		wasValidDrop: function(){
+			if(drop.category!=null){
+				drop.category = null;	
+				return vd;
+			}else{
+				return false;
+			}
+			
+		},
+		
+	}
+})
+
+
+
 .directive('draggable', function(dragDrop) {
 	return {
 		scope: {
@@ -15,17 +83,20 @@ angular.module('al.sortable', [])
 				function(e){
 					e.stopPropagation();
 				e.dataTransfer.effectAllowed = 'move'; //could make it copyMove later
-				if(scope.draggableType=="pub"){
-					e.dataTransfer.setData('Text', JSON.stringify(scope.pub));
+				dragDrop.setDragVars(scope.pub, scope.category, scope.parentCategory, scope.draggableType);
+				var dragItem = dragDrop.getDragItem();
+				e.dataTransfer.setData('Text', JSON.stringify(dragItem));
+				// if(scope.draggableType=="pub"){
+				// 	e.dataTransfer.setData('Text', JSON.stringify(scope.pub));
 
-				}
-				else if(scope.draggableType=="category"){
-					e.dataTransfer.setData('Text', JSON.stringify(scope.category));
-					scope.category.gotDropped=false;
+				// }
+				// else if(scope.draggableType=="category"){
+				// 	e.dataTransfer.setData('Text', JSON.stringify(scope.category));
+				// 	scope.category.gotDropped=false;
 
-				}
+				// }
 				
-				scope.parentCategory.gotDropped=false;
+				// scope.parentCategory.gotDropped=false;
 				this.classList.add('drag');
 				return false;
 			});
@@ -34,9 +105,11 @@ angular.module('al.sortable', [])
 				this.classList.remove('drag');
 				e.stopPropagation();
 
-				var categoryGotDropped = scope.category ? scope.category.gotDropped : false;
-				if(!scope.parentCategory.gotDropped && !categoryGotDropped){
-					if(scope.draggableType=="pub"){
+				// var categoryGotDropped = scope.category ? scope.category.gotDropped : false;
+				var drag = dragDrop.getDrag();
+				var drop = dragDrop.getDrop();
+				if(drop.category.title!=drag.parent.title){
+					if(drag.type=="pub"){
 
 						scope.$apply(function(){
 							for (var i=0; i<scope.parentCategory.publications.length; i++){
@@ -46,8 +119,9 @@ angular.module('al.sortable', [])
 							}
 						});
 					}
-					else if(scope.draggableType=="category"){
+					else if(drag.type=="category" && drop.category.title!=drag.category.title){
 						if(dragDrop.wasValidDrop()){
+
 							scope.$apply(function(){
 								for (var i=0; i<scope.parentCategory.subcategories.length; i++){
 									if(scope.parentCategory.subcategories[i].title == scope.category.title){
@@ -63,40 +137,7 @@ angular.module('al.sortable', [])
 }
 }
 })
-.service('dragDrop', function(){
-	
-	var vd = true;
-	var rd = false;
-	return {
-		isValidDrop : function(drag, drop){
-			var dragDrop = true;
-			if(drag.title == drop.title){
-				dragDrop = false;
-			}else{
-				for(var i=0; i<drag.subcategories.length; i++){
 
-					if(dragDrop){
-						dragDrop = this.isValidDrop(drop, drag.subcategories[i]);
-					}
-				}
-			}
-			vd = dragDrop;
-			return dragDrop;
-		},
-		wasValidDrop: function(){
-			if(rd){
-				rd=false;	
-				return vd;
-			}else{
-				return false;
-			}
-			
-		},
-		receivedDrop: function(bool){
-			rd= true;
-		}
-	}
-})
 
 
 .directive('droppable', function(dragDrop) {
@@ -149,31 +190,40 @@ angular.module('al.sortable', [])
 
 			        this.classList.remove('over');
 
-			        scope.category.gotDropped=true;
-			        dragDrop.receivedDrop();
+			        // scope.category.gotDropped=true;
+			        // dragDrop.receivedDrop();
+			        dragDrop.setDropVars(scope.category);
 			        var item =JSON.parse(e.dataTransfer.getData('Text'));
-			        if(item.hasOwnProperty('id')){
+			        var dragType = dragDrop.getDrag().type;
+			        if(dragType=="pub"){
 			        	// console.log("publication: " + item.id+ " dropped in category: " + scope.category.title);
 
 			        	scope.$apply(function(){
 			        		
-
+			        		var sorta = dragDrop.getSortaDrop();
 			        		//add the new item at the right place, saving that index
-			        		if(!scope.category.dropIndex){
-			        			scope.category.dropIndex = scope.category.publications.length;
-			        			scope.category.publications.push(item.id);
-			        		}else if(scope.category.dropIndex >scope.category.publications.length){
-				        		scope.category.dropIndex = scope.category.publications.length;
-	        					scope.category.publications.push(item.id);
-				        	}else{
-			        			scope.category.publications.splice(scope.category.dropIndex, 0, item.id);
+			        		if(sorta.category!=null && sorta.category.title==scope.category.title && sorta.index<scope.category.publications.length){
+			        			scope.category.publications.splice(sorta.index, 0, item.id);
 			        		}
+			        		else{
+			        			sorta.index = scope.category.publications.length;
+			        			scope.category.publications.push(item.id);
+			        		}
+			        		// if(sorta.index==null){
+			        		// 	scope.category.dropIndex = scope.category.publications.length;
+			        		// 	scope.category.publications.push(item.id);
+			        		// }else if(scope.category.dropIndex >scope.category.publications.length){
+				        	// 	scope.category.dropIndex = scope.category.publications.length;
+	        				// 	scope.category.publications.push(item.id);
+				        	// }else{
+			        		// 	scope.category.publications.splice(scope.category.dropIndex, 0, item.id);
+			        		// }
 
 
 			        		//find any duplicates (there should only be one)
 			        		var dupIndex = null
 			        		for(var i=0; i<scope.category.publications.length; i++){
-			        			if(scope.category.publications[i] == item.id && i!=scope.category.dropIndex){
+			        			if(scope.category.publications[i] == item.id && i!=sorta.index){
 			        				dupIndex=i;
 			        			}
 			        		}
@@ -182,39 +232,50 @@ angular.module('al.sortable', [])
 			        		if(dupIndex!=null){
 			        			scope.category.publications.splice(dupIndex,1);
 			        		}
-			        		delete scope.category.dropIndex;
-
+			        		dragDrop.setSortaDropVars(null, null);
 
 			        	});
 			        	
 
-			        }else if(item.hasOwnProperty('subcategories')){
+			        }else if(dragType=="category"){
 			        	// console.log("category: " + item.title+ " dropped in category: " +scope.category.title);
 			        	
 
 			        	scope.$apply(function(){
+
 			        		if(dragDrop.isValidDrop(item, scope.category)){
+			        			// console.log("is valid drop");
 			        			// console.log("was valid drop");
 
 
 					        	//add the new item at the right place, saving that index
-					        	if(!scope.category.dropIndex){
-					        		// console.log('no drop index');
-					        		scope.category.dropIndex = scope.category.subcategories.length;
-					        		scope.category.subcategories.push(item);
-					        	}else if(scope.category.dropIndex >scope.category.subcategories.length){
-					        		scope.category.dropIndex = scope.category.subcategories.length;
-					        		scope.category.subcategories.push(item);
-					        	}else{
-					        		// console.log('drop index: ' + scope.category.dropIndex);
-					        		scope.category.subcategories.splice(scope.category.dropIndex, 0, item);
-					        	}
+					        	// if(!scope.category.dropIndex){
+					        	// 	// console.log('no drop index');
+					        	// 	scope.category.dropIndex = scope.category.subcategories.length;
+					        	// 	scope.category.subcategories.push(item);
+					        	// }else if(scope.category.dropIndex >scope.category.subcategories.length){
+					        	// 	scope.category.dropIndex = scope.category.subcategories.length;
+					        	// 	scope.category.subcategories.push(item);
+					        	// }else{
+					        	// 	// console.log('drop index: ' + scope.category.dropIndex);
+					        	// 	scope.category.subcategories.splice(scope.category.dropIndex, 0, item);
+					        	// }
+
+					        		var sorta = dragDrop.getSortaDrop();
+			        		//add the new item at the right place, saving that index
+			        		if(sorta.category!=null && sorta.category.title==scope.category.title && sorta.index<scope.category.subcategories.length){
+			        			scope.category.subcategories.splice(sorta.index, 0, item);
+			        		}
+			        		else{
+			        			sorta.index = scope.category.subcategories.length;
+					        	scope.category.subcategories.push(item);
+			        		}
 
 
 				        		//find any duplicates (there should only be one)
 				        		var dupIndex = null
 				        		for(var i=0; i<scope.category.subcategories.length; i++){
-				        			if(scope.category.subcategories[i].title == item.title && i!=scope.category.dropIndex){
+				        			if(scope.category.subcategories[i].title == item.title && i!=sorta.index){
 				        				dupIndex=i;
 				        			}
 				        		}
@@ -223,7 +284,7 @@ angular.module('al.sortable', [])
 				        		if(dupIndex!=null){
 				        			scope.category.subcategories.splice(dupIndex,1);
 				        		}
-				        		delete scope.category.dropIndex;
+				        		dragDrop.setSortaDropVars(null, null);
 				        		
 				        	}
 				        });
@@ -239,7 +300,7 @@ false
 }
 })
 
-.directive('sortaDroppable', function() {
+.directive('sortaDroppable', function(dragDrop) {
 	return {
 		scope: {
 			parentCategory: "=",
@@ -268,7 +329,7 @@ false
 			el.addEventListener(
 				'drop',
 				function(e) {
-					scope.parentCategory.dropIndex = scope.index;
+					dragDrop.setSortaDropVars(scope.index, scope.parentCategory);
 					this.classList.remove('sorta-over');
 					return false;
 				},
